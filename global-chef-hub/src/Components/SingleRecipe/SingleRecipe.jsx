841e776688f9954ddee14576ee46730745ce5235
debabrataChakraborty <debabrata15-674@diu.edu.bd>
2023-05-04 22:07:40 +0600

    apikey problem solved

---
+++ global-chef-hub/src/Components/SingleRecipe/SingleRecipe.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable react/prop-types */
+
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { Button } from 'react-bootstrap';
+import { toast } from "react-toastify";
+
+function SingleRecipeComponent(props) {
+    function handleFav(event){
+       toast.success("added to Fav",{
+        position:toast.POSITION.TOP_RIGHT
+      })
+       event.target.disabled = true
+    }
+    console.log(props);
+    return (
+        <>
+        <div className="col">
+            
+            <Card>
+                <Card.Img variant="top" height={300} src={props.recipe.banner_image} />
+                <Card.Body>
+                    <h2>{props.recipe.name}</h2>
+                    <ListGroup variant="flush">
+                    <ListGroup.Item>
+                    <Card.Header>Ingredients</Card.Header>
+
+                    <ul>
+                        {props.recipe.ingredients.map((ingredient, index) => (
+                            <li className="ingredientItems" key={index}>{ingredient}</li>
+                        ))}
+                    </ul>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                    <Card.Header>Method</Card.Header>
+                    <ul className='recipe-method'>
+                        {props.recipe.recipe_method.split("\n").map((method, index) => (
+                            <li key={index}>{method}</li>
+                        ))}
+                    </ul>
+                    </ListGroup.Item>
+                    </ListGroup>
+
+                    <Button variant="warning" onClick={handleFav}>Add To Favourite</Button>{' '}
+                </Card.Body>
+            </Card>
+        </div>
+        </>
+    );
+}
+
+export default SingleRecipeComponent;
--- global-chef-hub/src/main.jsx
+++ global-chef-hub/src/main.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HomePage from "./Pages/Home";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Detials from "./Pages/Details";
+import { AuthProvider } from "./Provider/AuthProvider";
+import RegisterForm from "./Pages/Register";
+import LoginForm from "./Pages/Login";
+import ProtectedRoute from "./Routes/ProtectedRoute";
+import Blog from "./Components/Blog/Blog";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+function allChefLoader({ request }) {
+  return fetch("https://global-chef-server-debabratachakraborty880-gmailcom.vercel.app/alldata", {
+    signal: request.signal,
+  });
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    loader: allChefLoader,
+  },
+  {
+    path: "details/:chefid",
+    element: <ProtectedRoute><Detials /></ProtectedRoute> ,
+    loader: allChefLoader,
+  },
+  {
+    path: "/register",
+    element: <RegisterForm />,
+  },
+  {
+    path: "/login",
+    element: <LoginForm />,
+  },
+  {
+    path: "/blog",
+    element: <Blog />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
