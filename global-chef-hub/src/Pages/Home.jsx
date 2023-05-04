c01791fb02dee555b0c1c2004071bf381faa1ed5
debabrataChakraborty <debabrata15-674@diu.edu.bd>
2023-05-04 18:49:12 +0600

    pages added

---
+++ global-chef-hub/src/Pages/Home.jsx
@@ -0,0 +1,24 @@
+import ChefNavbar from "../Components/Header/Header";
+
+import Banner from "../Components/Banner/Banner";
+import AllChefComponent from "../Components/AllChef/AllChef";
+import Specialised from "../Components/SpeciledSection/Specialised";
+import AboutUs from "../Components/About/About";
+import Footer from "../Components/Footer/Footer";
+
+export default function HomePage() {
+
+  return (
+    <>
+      <ChefNavbar />
+      <Banner />
+      <AllChefComponent/>
+      <hr />
+      <Specialised/>
+      <hr />
+      <AboutUs/>
+     
+      <Footer/>
+    </>
+  );
+}
---
+++ global-chef-hub/src/Provider/AuthProvider.jsx
@@ -0,0 +1,53 @@
+/* eslint-disable react/prop-types */
+import { createContext,useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, signInWithPopup,onAuthStateChanged, GithubAuthProvider, GoogleAuthProvider  } from "firebase/auth"
+import {auth} from "../Firebase/Firebase.config"
+export const AuthContext = createContext(null); 
+const githubAuthProvider = new GithubAuthProvider();
+const googleAuthProvider = new GoogleAuthProvider();
+
+
+
+export function AuthProvider({children}){
+    const [user, setuser] = useState(null)
+    const [loading, setloading] = useState(true)
+    
+    function SignUp(email, password){
+        setloading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+    function Login(email, password){
+        setloading(true)
+        return signInWithEmailAndPassword(auth,email,password)    
+    }
+    function LogOut(){
+        setloading(true)
+        return signOut(auth)
+    }
+    function signInWithGithub(){
+        setloading(true)
+        return signInWithPopup(auth,githubAuthProvider)
+    }
+    function signInWithGoogle(){
+        setloading(true)
+        return signInWithPopup(auth,googleAuthProvider)
+    }
+
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth,(currentuser) => {
+            console.log('auth state change', currentuser);
+            setuser(currentuser);
+            setloading(false)
+        });
+        return unsubscribe;
+      }, []);
+    
+      const value = {
+        user,loading,SignUp,Login,LogOut,signInWithGithub,signInWithGoogle
+      };
+
+
+
+    return <AuthContext.Provider  value={{...value}}>{children}</AuthContext.Provider>
+}
